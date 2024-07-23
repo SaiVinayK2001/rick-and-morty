879e87c3071de6b603fde15e142e12862199ff94
saivinay <kanamarlapudisaivinay@gmail.com>
2024-07-23 10:19:02 +0530

    Comn

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import APIComponent from './Components/APIComponent'
+
+const App = () => {
+  return (
+    <div>
+      <APIComponent />
+    </div>
+  )
+}
+
+export default App
